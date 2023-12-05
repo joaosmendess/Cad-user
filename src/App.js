133d42911f87691c2e5d22a089c825cb5b500fb1
joaosmendess <joaosilva0721@gmail.com>
2023-12-04 23:29:29 -0300

    Criando Container e seu estilo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import GlobalStyle from "./styles/global";
+import {toast, ToastContainer} from 'react-toastify'
+
+import styled from "styled-components";
+
+const Container = styled.div `
+
+width: 100%;
+max-width: 800px;
+margin-top: 20px;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 10px;
+
+
+`;
 
 function App() {
   return (
-    <div >
-      <header >
-     <h1>cara , hello world</h1>
-      </header>
-    </div>
+    < >
+    <Container>
+
+      <h1>olas</h1>
+    </Container>
+    <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+     
+   <GlobalStyle/>
+      
+    </>
   );
 }
 
